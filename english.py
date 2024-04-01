5d40cc5e76c127ac6db209d4ab4e227402ac508c
Md. Nahid Ahsan <42906020+Nahid-Ahsan@users.noreply.github.com>
2024-04-01 12:15:39 +0600

    Update english.py
    update en

--- english.py
+++ english.py
@@ -6,7 +6,7 @@ model = AutoModelForSeq2SeqLM.from_pretrained("csebuetnlp/banglat5_nmt_en_bn")
 tokenizer = AutoTokenizer.from_pretrained("csebuetnlp/banglat5_nmt_en_bn", use_fast=False)
 
 
-def translate(input_sentence):
+def translate_en(input_sentence):
     input_ids = tokenizer(normalize(input_sentence), return_tensors="pt").input_ids
     generated_tokens = model.generate(input_ids)
     decoded_tokens = tokenizer.batch_decode(generated_tokens)[0]
