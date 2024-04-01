e41b21e8996920e3ad8602439ffada711760a2fd
Md. Nahid Ahsan <42906020+Nahid-Ahsan@users.noreply.github.com>
2024-04-01 12:18:50 +0600

    Update main.py
    update apis

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from flask import Flask, request, jsonify
 from bangla import *
+from english import *
 import gunicorn
 app = Flask(__name__)
 
 
-@app.route('/response', methods=['POST'])
+@app.route('/bangla', methods=['POST'])
 def queryresonse():
     try:
         query = request.json.get('query', '')
         # print(query)  
-        result = translate(query)
+        result = translate_bn(query)
         return jsonify({'result': result})
     except Exception as e:
         return jsonify({'error': str(e)})
 
-
+@app.route('/english', methods=['POST'])
+def queryresonse():
+    try:
+        query = request.json.get('query', '')
+        # print(query)  
+        result = translate_en(query)
+        return jsonify({'result': result})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+	    
 if __name__ == "__main__":
 	gunicorn -w 4 -b 0.0.0.0:5000 main:app
 	app.run(debug=True)
